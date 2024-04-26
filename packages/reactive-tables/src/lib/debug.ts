42074c9c87cf0e9caeea0b9c12eac19c4ae807b2
polarzero <0xpolarzero@gmail.com>
2024-04-26 23:31:56 +0200

    test: add debug & verbose tests

--- packages/reactive-tables/src/lib/debug.ts
+++ packages/reactive-tables/src/lib/debug.ts
@@ -6,8 +6,3 @@ export const error = createDebug("primodium:reactive-tables");
 debug.log = console.debug.bind(console);
 // Pipe error output to stderr
 error.log = console.error.bind(console);
-
-if (process.env.DEBUG) {
-  debug.enabled = true;
-  error.enabled = true;
-}
