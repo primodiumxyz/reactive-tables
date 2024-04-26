1ef6ca2c75b13be799d6084077ae66c101a3c24f
polarzero <0xpolarzero@gmail.com>
2024-04-26 23:18:27 +0200

    chore: rename package to reactive-tables

--- packages/reactive-tables/src/lib/debug.ts
+++ packages/reactive-tables/src/lib/debug.ts
@@ -1,7 +1,7 @@
 import createDebug from "debug";
 
-export const debug = createDebug("primodium:tiny-base-wrapper");
-export const error = createDebug("primodium:tiny-base-wrapper");
+export const debug = createDebug("primodium:reactive-tables");
+export const error = createDebug("primodium:reactive-tables");
 // Pipe debug output to stdout instead of stderr
 debug.log = console.debug.bind(console);
 // Pipe error output to stderr
