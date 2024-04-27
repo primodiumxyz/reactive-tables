0e551c733de084eb45ba58a4b40da1292257b6b8
polarzero <0xpolarzero@gmail.com>
2024-04-27 18:34:33 +0200

    chore: export recordToHexKeyTuple

--- packages/reactive-tables/src/utils.ts
+++ packages/reactive-tables/src/utils.ts
@@ -8,6 +8,8 @@ export {
   getPropertiesSchema,
   // Concat a tuple of hex keys into a single record
   hexKeyTupleTo$Record,
+  // Convert a record into a tuple of hex keys
+  $recordToHexKeyTuple,
   // Convert ABI types to TypeScript understandable types
   schemaAbiTypeToRecsType,
 } from "@/lib";
